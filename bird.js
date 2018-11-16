75779234a8ce9685a8edc205dcd2033dd8375a14
Meet Shah <meetshah7202@gmail.com>
2018-11-16 18:47:41 +0530

    Update bird.js

--- bird.js
+++ bird.js
@@ -1,5 +1,4 @@
 
-// Mutation function to be passed into bird.brain
 function mutate(x) {
     if (random(1) < 0.1) {
       let offset = randomGaussian() * 0.5;
@@ -12,20 +11,16 @@ function mutate(x) {
   
   class Bird {
     constructor(brain) {
-      // position and size of bird
       this.x = 64;
       this.y = height / 2;
       this.r = 12;
   
-      // Gravity, lift and velocity
       this.gravity = 0.8;
       this.lift = -12;
       this.velocity = 0;
 
       this.icon = birdSprite;
   
-      // Is this a copy of another Bird or a new one?
-      // The Neural Network is the bird's "brain"
       if (brain instanceof NeuralNetwork) {
         this.brain = brain.copy();
         this.brain.mutate(mutate);
@@ -33,30 +28,22 @@ function mutate(x) {
         this.brain = new NeuralNetwork(5, 8, 2);
       }
   
-      // Score is how many frames it's been alive
       this.score = 0;
-      // Fitness is normalized version of score
       this.fitness = 0;
     }
   
-    // Create a copy of this bird
     copy() {
       return new Bird(this.brain);
     }
   
-    // Display the bird
     show() {
       
       fill(169,169,169);
       stroke(255);
       ellipse(this.x, this.y, this.r * 2, this.r * 2);
-      //image(this.icon, this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
     }
   
-    // This is the key function now that decides
-    // if it should jump or not jump!
     think(pipes) {
-      // First find the closest pipe
       let closest = null;
       let record = Infinity;
       for (let i = 0; i < pipes.length; i++) {
@@ -68,45 +55,32 @@ function mutate(x) {
       }
   
       if (closest != null) {
-        // Now create the inputs to the neural network
         let inputs = [];
-        // x position of closest pipe
         inputs[0] = map(closest.x, this.x, width, 0, 1);
-        // top of closest pipe opening
         inputs[1] = map(closest.top, 0, height, 0, 1);
-        // bottom of closest pipe opening
         inputs[2] = map(closest.bottom, 0, height, 0, 1);
-        // bird's y position
         inputs[3] = map(this.y, 0, height, 0, 1);
-        // bird's y velocity
         inputs[4] = map(this.velocity, -5, 5, 0, 1);
   
-        // Get the outputs from the network
         let action = this.brain.predict(inputs);
-        // Decide to jump or not!
         if (action[1] > action[0]) {
           this.up();
         }
       }
     }
   
-    // Jump up
     up() {
       this.velocity += this.lift;
     }
   
     bottomTop() {
-      // Bird dies when hits bottom?
       return (this.y > height || this.y < 0);
     }
   
-    // Update bird's position based on velocity, gravity, etc.
     update() {
       this.velocity += this.gravity;
-      // this.velocity *= 0.9;
       this.y += this.velocity;
   
-      // Every frame it is alive increases the score
       this.score++;
     }
   }
